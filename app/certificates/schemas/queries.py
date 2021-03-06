21c3f2faaa84342d2fa95709293bc84d1e2a23ae
Mehmet Ali Peker <maps6134@hotmail.com>
2021-03-06 04:29:09 +0300

    certificates by slug & certificate categories graphql queries added

--- app/certificates/schemas/queries.py
+++ app/certificates/schemas/queries.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 from app.common.schemas.languages import LanguageNode
-from app.certificates.models import Certificate
+from app.certificates.models import Certificate, CertificateCategory
 
 
 # queries
@@ -19,11 +19,28 @@ class CertificateNode(DjangoObjectType):
             "name": ["exact", "icontains", "istartswith"],
         }
 
+class CertificateCategoryNode(DjangoObjectType):
+    # id = graphene.ID(source="pk", required=True)
+
+    class Meta:
+        model = CertificateCategory
+        exclude = ("uuid",)
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+        }
+
 
 class Query(graphene.ObjectType):
     certificate = graphene.relay.Node.Field(CertificateNode)
+    certificate_by_slug = graphene.Field(CertificateNode, slug=graphene.String(required=True))
     certificate_by_uuid = graphene.Field(CertificateNode, uuid=graphene.UUID(required=True))
     certificates_all = DjangoFilterConnectionField(CertificateNode)
+
+    certificate_category = graphene.relay.Node.Field(CertificateCategoryNode)
+    certificate_category_by_slug = graphene.Field(CertificateCategoryNode, slug=graphene.String(required=True))
+    certificate_category_by_uuid = graphene.Field(CertificateCategoryNode, uuid=graphene.UUID(required=True))
+    certificate_categories_all = DjangoFilterConnectionField(CertificateCategoryNode)
 
     @staticmethod
     # pylint:disable=unused-argument
